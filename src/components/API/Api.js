968dd95426ecc216431ccc9365710fa231749dd1
ivelceva <73798374+ivelceva@users.noreply.github.com>
2023-06-19 14:25:33 -0700

    Update Api.js

--- src/components/API/Api.js
+++ src/components/API/Api.js
@@ -1,20 +1,22 @@
 import axios from 'axios';
 
-const instanceContacts = axios.create({
-  baseURL: 'https://6490a4101e6aa71680cb98bb.mockapi.io/contacts',
-});
+axios.defaults.baseURL = 'https://6490a4101e6aa71680cb98bb.mockapi.io/contacts/';
+
+// const instanceContacts = axios.create({
+//   baseURL: 'https://6490a4101e6aa71680cb98bb.mockapi.io/contacts',
+// });
 
 export const getContacts = async () => {
-  const { data } = await instanceContacts.get('/');
+  const { data } = await axios.get('/');
   return data;
 };
 
 export const addContact = async data => {
-  const { data: result } = await instanceContacts.post('/', data);
+  const { data: result } = await axios.post('/', data);
   return result;
 };
 
 export const deleteContact = async id => {
-  const { data } = await instanceContacts.delete(`/${id}`);
+  const { data } = await axios.delete(`/${id}`);
   return data;
 };
