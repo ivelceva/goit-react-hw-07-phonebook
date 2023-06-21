2732c890fb254d607e1b56696b6e7d079614c2cc
ivelceva <73798374+ivelceva@users.noreply.github.com>
2023-06-21 08:09:35 -0700

    clean

--- src/components/contactList/ContactList.jsx
+++ src/components/contactList/ContactList.jsx
@@ -33,36 +33,4 @@ const ContactList = () => {
 
 export default ContactList;
 
-// import UserContact from '../userContact/UserContact';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { getContacts, getFilter } from 'redux/selectors';
-// import { contactActions } from '../../redux/contactSlice';
-// import css from './ContactList.module.css';
 
-// const ContactList = () => {
-//   const contacts = useSelector(getContacts);
-//   const dispatch = useDispatch();
-//   const filter = useSelector(getFilter);
-
-//   const contactSearch = contacts.filter(({ name }) => {
-//       return name.toLowerCase().includes(filter.toLowerCase());
-//   });
-
-//   return (
-//     <ul className={css.list}>
-//       {contactSearch.map(({ name, number, id }) => (
-//         <li className={css.user} key={id}>
-//           <UserContact name={name} number={number} />
-//           <button
-//             className={css.btn}
-//             onClick={() => dispatch(contactActions.deleteContact(id))}
-//           >
-//             Delete
-//           </button>
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// };
-
-// export default ContactList;
